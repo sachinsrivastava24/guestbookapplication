d385d89e7925443a3f5465a40d055e3844c2e0ef
Sachin Srivastava <sonu.srivastava24@gmail.com>
2022-06-03 23:55:08 +0100

    Initial level commit

--- src/main/java/guestbook/Application.java
+++ src/main/java/guestbook/Application.java
@@ -60,11 +60,15 @@ public class Application {
 		return args -> {
 
 			Stream.of( //
-					new GuestbookEntry("H4xx0r", "first!!!"), //
+					/*new GuestbookEntry("H4xx0r", "first!!!"), //
 					new GuestbookEntry("Arni", "Hasta la vista, baby"), //
 					new GuestbookEntry("Duke Nukem", "It's time to kick ass and chew bubble gum. And I'm all out of gum."), //
 					new GuestbookEntry("Gump1337",
-							"Mama always said life was like a box of chocolates. You never know what you're gonna get.")) //
+							"Mama always said life was like a box of chocolates. You never know what you're gonna get.")) *///
+							new GuestbookEntry("Sachin", "FirstEntry"), //
+							new GuestbookEntry("Roshan", "2nd Entry"), //
+							new GuestbookEntry("Kundan", "Third Entry"), //
+							new GuestbookEntry("Anil","Fourth Entry"))
 					.forEach(guestbook::save);
 		};
 	}
